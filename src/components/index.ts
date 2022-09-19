e47b03385ef4cb2fdf24a1fcb9ac8293c846ab6c
ScriptCamilo <script.camilo@gmail.com>
2022-09-19 12:22:35 -0300

    feat: web project finished

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as GameCard } from  './GameCard';
 export { default as CreateAdBanner } from './CreateAdBanner';
+export { default as CreateAdModal } from './CreateAdModal';
 export { default as Input } from './Input';
